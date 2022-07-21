fc1bcc1644484b16a10eeda6105271ec06a0ab23
MuhammadFahrial <rialjhe111@gmail.com>
2022-07-21 15:11:37 +0800

    ur message here

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const icon = document.getElementById('icon');
+const nav = document.querySelector('.nav-item');
+
+
+icon.addEventListener('click', function(){
+    nav.classList.toggle('active');
+});
+
+
