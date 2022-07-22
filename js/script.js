9b1bb75c8e645ab81205d86019625bf3d0298adf
MuhammadFahrial <rialjhe111@gmail.com>
2022-07-22 08:40:49 +0800

    transisi

--- js/script.js
+++ js/script.js
@@ -1,9 +1,23 @@
 const icon = document.getElementById('icon');
 const nav = document.querySelector('.nav-item');
+const jumbotronI = document.querySelector('.jumbotron-information');
+const jumbotronImg = document.querySelector('.jumbotron-img');
+const skill = document.getElementById('container-skill');
+
+console.log(skill);
+window.addEventListener('load', function(){
+    jumbotronI.classList.toggle('active-jumbotron-information');
+
+    jumbotronImg.classList.toggle('active-jumbotron-img');
+
+    skill.classList.toggle('container-active');
+});
 
 
 icon.addEventListener('click', function(){
     nav.classList.toggle('active');
 });
+
+
 
 
